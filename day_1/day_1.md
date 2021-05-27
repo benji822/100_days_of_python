5ee580e89c2acaaf7fa15b44f108a464a95ad53d
ben <ben>
2021-05-27 21:28:47 +0800

    rebase main branch

---
+++ day_1/day_1.md
@@ -0,0 +1,3 @@
+# Working with variables in python to manage data
+
+Create a simple console app to getting data from user and show the manipulate data to user
---
+++ day_1/day_1.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Hello welcome to band name genarator app!!")
+#2. Ask the user for the city that they grew up in.
+city = input("Please enter the city name you're growing up:\n")
+#3. Ask the user for the name of a pet.
+pet = input("Please enter your pet name:\n")
+#4. Combine the name of their city and pet and show them their band name.
+print(f"The band name is {city} {pet}")
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
