2b915e506c51c0c63e88e2e0ca96023097682b88
ben <ben>
2021-06-07 14:05:47 +0800

    completed day 3 exercise, working on day 3 project

--- day_2/tip_calculator.py
+++ day_2/tip_calculator.py
@@ -5,3 +5,10 @@
 #HINT 1: https://www.google.com/search?q=how+to+round+number+to+2+decimal+places+python&oq=how+to+round+number+to+2+decimal
 #HINT 2: https://www.kite.com/python/answers/how-to-limit-a-float-to-two-decimal-places-in-python
 
+print("Welcome to the tips calculator!")
+total_bill = float(input("What is the total billed amount? $"))
+tips_percent = int(input("How many percent of tips you would like to give? 10, 12, or 15? "))
+total_person = int(input("How many people would like to split the bill? "))
+
+bill_with_tips_per_person = (total_bill / total_person) * (tips_percent / 100 + 1)
+print(f"Bill for each person are: ${bill_with_tips_per_person:.2f}")
